3bc2c6597bc6bcf9c72609ac7a96a6ef03036d5e
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-06-03 18:23:55 +0530

    starter files & db schema

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+
+
+  return (
+    <div className='bg-rose-500 text-white'>
+      <p>Hello</p>
+    </div>
+  )
+}
+
+export default App
---
+++ task_app/task_app/doctype/actions/test_actions.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, shoumya and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestActions(FrappeTestCase):
+	pass
