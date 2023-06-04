24f71dec0df12963a8098d008b100a1cb6c712a4
CoffeeGeek101 <workwithshoumya@gmail.com>
2023-06-05 00:14:53 +0530

    improved code quality

--- frontend/src/components/tasklist/StatusbasedList.tsx
+++ frontend/src/components/tasklist/StatusbasedList.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Check, Edit3, Trash } from 'lucide-react'
 import { task } from '../../api/api';
 import { useAppDispatch } from '../../redux/store';
 import { openUpdateModal } from '../../redux/UpdateModalSlice';
 import axios from 'axios';
+import TaskCard from './TaskCard';
 
 interface StatusbasedListProps {
-    status: string;
+    status ?: string;
     tasklist: task[];
 }
 
@@ -55,24 +55,7 @@ const StatusbasedList: React.FC<StatusbasedListProps> = ({ status, tasklist }) =
                         :
                         statusbasedlist.map((task) => (
                             <>
-                                <div className={`${task.status === 'Archived' ? 'bg-slate-400/30 cursor-not-allowed' : ''} w-full h-auto flex flex-col bg-white p-5 rounded-md shadow-sm gap-3 cursor-pointer`}>
-                                    <div className='flex flex-row justify-between items-center'>
-                                        <div className='flex flex-row items-center gap-3'>
-                                            <div className='bg-blue-300/50 text-sm font-medium w-[80px] h-[30px] flex flex-row items-center justify-center rounded-md '>{task.category}</div>
-                                            <div onClick={() => handleUpdateModal(task)} className='bg-slate-200/50 p-1 rounded-md'><Edit3 size={17}/></div>
-                                        </div>
-                                        <div className='flex flex-row items-center gap-3'>
-                                            <div
-                                            onClick={() => handleStatusChange(task)}
-                                            className={`bg-green-300/50 text-sm font-medium w-[80px] h-[30px] flex flex-row items-center justify-center rounded-md ${status === 'Todo' ? 'cursor-pointer' : 'bg-slate-500/20 pointer-events-none cursor-not-allowed'}`}><Check /></div>
-                                            <div 
-                                            onClick={() => handleArchive(task)}
-                                            className={`bg-rose-300/50 text-sm font-medium w-auto h-[30px] p-2 flex flex-row items-center justify-center rounded-md ${status === 'Archived' ? 'bg-slate-500/20 pointer-events-none cursor-not-allowed' : 'cursor-pointer'}`}><Trash size={18} /></div>
-                                        </div>
-                                    </div>
-                                    <p className='font-semibold'>{task.title}</p>
-                                    <p>{task.description}</p>
-                                </div>
+                                <TaskCard status={status} task={task} archive={handleArchive} statusChange={handleStatusChange} updateModal={handleUpdateModal}/>
                             </>
                         ))
                 }
